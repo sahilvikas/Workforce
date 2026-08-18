850ab63db3948a06131fc4b97b06edd7fb055f08
Vamshi <vamshiharish@cozycornerpatio.com>
2026-08-18 22:43:04 +0530

    feat: workforce hub - 4 tabs, shared components, ERPNext API integration

---
+++ workforce/public/js/workforce_hub.bundle.js
@@ -0,0 +1,5 @@
+import WorkforceHub from './components/WorkforceHub.vue';
+
+if (!window.workforce) window.workforce = {};
+if (!window.workforce.ui) window.workforce.ui = {};
+window.workforce.ui.WorkforceHub = WorkforceHub;

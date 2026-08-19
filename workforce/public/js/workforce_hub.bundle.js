5c0ff126565733bf0cce99c31baf4ffe61efcbc2
Vamshi <vamshiharish@cozycornerpatio.com>
2026-08-19 09:15:41 +0530

    Revert "feat: workforce hub - 4 tabs, shared components, ERPNext API integration"
    This reverts commit 850ab63db3948a06131fc4b97b06edd7fb055f08.

--- workforce/public/js/workforce_hub.bundle.js
+++
@@ -1,5 +0,0 @@
-import WorkforceHub from './components/WorkforceHub.vue';
-
-if (!window.workforce) window.workforce = {};
-if (!window.workforce.ui) window.workforce.ui = {};
-window.workforce.ui.WorkforceHub = WorkforceHub;
